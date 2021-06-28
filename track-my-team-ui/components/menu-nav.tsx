46551b45cf780d170cffb5a0de150cc54f1f9abb
Fernando Piccin <fernando.piccin@wiggle.com>
2021-06-28 22:06:47 +0100

    Added link to app title in the menu

--- track-my-team-ui/components/menu-nav.tsx
+++ track-my-team-ui/components/menu-nav.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Link from "next/link";
 interface MenuNavProps {}
 export const MenuNav: React.FC<MenuNavProps> = (props) => {
   return (
     <div className="header">
-      <span>Track my team</span>
+      <Link href="/">
+        <a>
+          <span>Track my team</span>
+        </a>
+      </Link>
     </div>
   );
 };
